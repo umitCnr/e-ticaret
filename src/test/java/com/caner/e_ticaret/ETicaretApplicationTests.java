4d6b912a184b22792fc765a0dea11c06cba96347
umitcaner <cancar27127@gmail.com>
2024-11-13 03:36:16 +0300

    first commit

---
+++ src/test/java/com/caner/e_ticaret/ETicaretApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caner.e_ticaret;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ETicaretApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
